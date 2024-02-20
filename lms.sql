ec35487efcbc7439aa9b49f2d33e7da05aa1b91d
TEJASAI KALLEPALLI <43806122+TejaRaavan@users.noreply.github.com>
2024-02-20 13:20:56 -0600

    database for lms

--- lms.sql
+++ lms.sql
@@ -1,16 +1,15 @@
 create database lms;
 USE  lms;
 create table member(
+memberID bigint,
 firstName varchar(30),
 lastName varchar(30),
-phoneNo bigint,
 emailId varchar(40),
-memberID bigint
+phoneNo bigint
 );
 create table book(
 bookID varchar(30),
 title varchar(30),
-price double,
-author varchar(40)
+author varchar(40),
+price double
 );
-select * from member;
